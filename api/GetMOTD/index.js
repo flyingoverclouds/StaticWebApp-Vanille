f7616bb10717e2e58b509e76e66a76b06bff0f86
Nicolas CLERC <nicolas.clerc@sablefin.net>
2021-04-19 09:46:38 +0200

    demo live

--- api/GetMOTD/index.js
+++ api/GetMOTD/index.js
@@ -1,7 +1,7 @@
 module.exports = async function (context, req) {
     context.res = {
       body: {
-        text: "Hello from the Demo à la vanille"
+        text: "Hello from the Demo à la vanille LIVE"
       }
     };
   };
